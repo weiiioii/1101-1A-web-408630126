6e5d3de93f266c1af1d3e4dcc013233308bb5448
weiiioii <408630126@gms.tku.edu.tw>
2021-11-19 13:40:32 +0800

    renew

---
+++ 4_mid-project/script.js
@@ -0,0 +1,18 @@
+function change(index) {
+    const p = document.querySelector('.show-classdemo');
+    console.log('p',p);
+    switch(index){
+        case 1:
+            p.innerHTML = '<iframe width=100% height=100% src="" />';
+
+            break;
+        case 2:
+            p.innerHTML = '<iframe width=100% height=100% src="" />';
+
+            break;
+        case 3:
+            p.innerHTML = '<iframe width=100% height=100% src="" />';
+
+            break;
+    }
+}
