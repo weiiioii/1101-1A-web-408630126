8e87194f5a8cb6c608abb4a3589ff9e10045f44b
freedomwen <vbpig0603@gmail.com>
2021-11-21 17:32:12 +0800

    renew

---
+++ 4_mid-project/md/mid-project/midproj.md
@@ -0,0 +1,33 @@
+[Github repo URL]()
+
+[Vercel app URL]()
+
+#a. (5%) Hero 頁面
+
+#####第 1 張
+
+![](https://i.imgur.com/jYfAtRm.png)
+
+#####第 2 張
+
+![](https://i.imgur.com/DwSV3ja.png)
+
+#####第 3 張
+
+![](https://i.imgur.com/P11wIgF.png)
+
+#####第 4 張
+
+![](https://i.imgur.com/PrbufcX.jpg)
+
+#####第 5 張
+
+![](https://i.imgur.com/dDYNZCt.jpg)
+
+#b. (5%) 履歷表
+
+![](https://i.imgur.com/A2PUlQo.png)
+
+#c. (5%) 個人圖片集 2 組
+
+![](https://i.imgur.com/F1JJvGb.jpg)
--- 4_mid-project/script.js
+++ 4_mid-project/script.js
@@ -1,18 +1,26 @@
 function change(index) {
-    const p = document.querySelector('.show-classdemo');
-    console.log('p',p);
-    switch(index){
-        case 1:
-            p.innerHTML = '<iframe width=100% height=100% src="" />';
-
-            break;
-        case 2:
-            p.innerHTML = '<iframe width=100% height=100% src="" />';
-
-            break;
-        case 3:
-            p.innerHTML = '<iframe width=100% height=100% src="" />';
-
-            break;
-    }
+  var hero_page = document.querySelector("#hero_page");
+  var resume_page = document.querySelector("#resume_page");
+  var gallery_page = document.querySelector("#gallery_page");
+  console.log("hero_page", hero_page);
+  console.log("resume_page", resume_page);
+  console.log("gallery_page", gallery_page);
+  switch (index) {
+    case 1:
+      hero_page.style.display = "inline";
+      resume_page.style.display = "none";
+      gallery_page.style.display = "none";
+      break;
+    case 2:
+      hero_page.style.display = "none";
+      resume_page.style.display = "inline";
+      gallery_page.style.display = "none";
+      break;
+    case 3:
+      hero_page.style.display = "none";
+      resume_page.style.display = "none";
+      gallery_page.style.display = "inline";
+      break;
+  }
 }
+change(1);
